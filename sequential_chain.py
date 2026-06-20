7cb5a58f97106c54f0958e413d5b47b22e31d36d
Jagdeep Singh <singh.jagdeep0504@gmail.com>
2026-06-20 16:13:47 -0400

    added project file

---
+++ sequential_chain.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+
+os.environ['LANGCHAIN_PROJECT']="Sequential LLM App"
+
+load_dotenv()
+
+prompt1=PromptTemplate(
+    template="Generate a detailed report on {topic}",
+    input_variables=['topic']
+)
+
+prompt2=PromptTemplate(
+    template="Generate a 5 pointer summary from the following text \n {text}",
+    input_variables=['text']
+)
+
+model = ChatGroq(model="llama-3.1-8b-instant")
+
+parser=StrOutputParser()
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result=chain.invoke({'topic': 'Geoffrey Hinton'})
+
+print(result)
